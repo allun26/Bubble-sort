3c4f1de0c929d6018d5c5c582089febf6cba0f25
Rodolfo <acosta.rodolfo.rca@gmail.com>
2019-08-08 17:45:39 -0500

    Fix linter issues

--- ruby.rb
+++ ruby.rb
@@ -1,37 +1,34 @@
 def bubble_sort(array)
-
-    need_sort = true
-    while need_sort
-        need_sort = false
-        for i in 0...array.length - 1
-            if array[i] > array[i + 1]
-                array[i], array[i + 1] = array[i + 1], array[i]
-                need_sort = true
-            end
-        end
+  need_sort = true
+  while need_sort
+    need_sort = false
+    for i in 0...array.length - 1
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        need_sort = true
+      end
     end
+  end
 end
 
 def bubble_sort_by(array)
-    need_sort = true
-    while need_sort
-        need_sort = false
-        for i in 0...array.length - 1
-            if yield(array[i], array[i + 1]) > 0
-            array[i], array[i + 1] = array[i + 1], array[i]
-            need_sort = true
-            end
-        end
+  need_sort = true
+  while need_sort
+    need_sort = false
+    for i in 0...array.length - 1
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        need_sort = true
+      end
     end
+  end
 end
 
-
-
-arr = [4,3,78,2,0,2]
+arr = [4, 3, 78, 2, 0, 2]
 arr1 = ["Hi", "Hello", "Hey"]
 
 bubble_sort(arr)
-bubble_sort_by(arr1) { |left, right| left.length - right.length } 
+bubble_sort_by(arr1) { |left, right| left.length - right.length }
 
 print arr
 print arr1
